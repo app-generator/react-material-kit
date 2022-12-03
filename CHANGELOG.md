a3857933347d89d6f040425e59574c41ca7ff1e6
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-03 16:15:14 +0200

    CleanUp before Update

--- CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# Change Log
-
-## [0.0.1] 2021-07-04
-### Improvements
-
-- Update Dependencies
-- Update License file
-- Add CHANGELOG.md to track all changes
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import reducers from "./reducers";
-import preloadedState from "./preloadedState";
-
-export default createStore(reducers, preloadedState, composeWithDevTools());
